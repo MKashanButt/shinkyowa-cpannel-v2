483f1c32b061e0bb07277581628abf5a8090bc0b
MKashanButt <kashanbutt566@gmail.com>
2025-05-30 00:26:22 +0500

    add category controller

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreCategoryRequest;
+use App\Http\Requests\UpdateCategoryRequest;
+use App\Models\Category;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+
+class CategoryController extends Controller
+{
+    public function index()
+    {
+        $categories = Category::withCount('stock')
+            ->paginate(8);
+        return view("category.index", compact('categories'));
+    }
+
+    public function create()
+    {
+        return view('category.create');
+    }
+
+    public function store(StoreCategoryRequest $request)
+    {
+        $validated = $request->validated();
+
+        Category::create($validated);
+
+        return redirect()->route('category.index')
+            ->with('success', 'Category created successfully.');
+    }
+
+    public function edit(Category $category)
+    {
+        return view('category.edit', compact('category'));
+    }
+
+    public function update(UpdateCategoryRequest $request, Category $category)
+    {
+        $validated = $request->validated();
+
+        $category->update($validated);
+
+        return redirect()->route('category.index')
+            ->with('success', 'Category updated successfully.');
+    }
+
+    public function destroy(Category $category)
+    {
+        $category->delete();
+
+        return redirect()->back()
+            ->with('success', 'Category Deleted');
+    }
+}
