d7f04731f0e27af2a5b2ac40218ad582f69c0896
MKashanButt <kashanbutt566@gmail.com>
2025-05-30 00:27:06 +0500

    add currency controller

---
+++ app/Http/Controllers/CurrencyController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreCurrencyRequest;
+use App\Http\Requests\UpdateCurrencyRequest;
+use App\Models\Currency;
+use Illuminate\Http\Request;
+
+class CurrencyController extends Controller
+{
+    public function index()
+    {
+        $currencies = Currency::withCount('stock')
+            ->paginate(8);
+        return view("currency.index", compact('currencies'));
+    }
+    public function create()
+    {
+        return view('currency.create');
+    }
+
+    public function store(StoreCurrencyRequest $request)
+    {
+        $validated = $request->validated();
+
+        Currency::create($validated);
+
+        return redirect()->route('currency.index')
+            ->with('success', 'Currency created successfully.');
+    }
+
+    public function edit(Currency $currency)
+    {
+        return view('currency.edit', compact('currency'));
+    }
+
+    public function update(UpdateCurrencyRequest $request, Currency $currency)
+    {
+        $validated = $request->validated();
+
+        $currency->update($validated);
+
+        return redirect()->route('currency.index')
+            ->with('success', 'Currency updated successfully.');
+    }
+
+    public function destroy(Currency $currency)
+    {
+        $currency->delete();
+
+        return redirect()->back()
+            ->with('success', 'Currency Deleted');
+    }
+}
