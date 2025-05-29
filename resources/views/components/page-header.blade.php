cefd7c284ebd1a959285d78b2ae69d4e64f33a50
MKashanButt <kashanbutt566@gmail.com>
2025-05-30 00:32:01 +0500

    remove page header component

--- resources/views/components/page-header.blade.php
+++
@@ -1,6 +0,0 @@
-<div class="flex justify-between items-center">
-    <h2 class="font-semibold text-xl leading-tight">
-        {{ $slot . 's' }}
-    </h2>
-    <x-secondary-button class="">{{ $slot }}</x-secondary-button>
-</div>
