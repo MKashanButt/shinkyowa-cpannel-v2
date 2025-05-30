a9dda1fe9c64fbf4213bc728e2c4d410f2438e80
MKashanButt <kashanbutt566@gmail.com>
2025-05-30 21:19:55 +0500

    add country request

---
+++ app/Http/Requests/UpdateCountryRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+
+class UpdateCountryRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return Auth::check() && Auth::user()->hasRole('admin');
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string', 'max:100', 'unique:categories,name' . $this->country->id],
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'name.required' => 'name is required',
+            'name.unique' => 'name should be unique',
+        ];
+    }
+}
