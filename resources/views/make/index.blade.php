618ed1be5392729a6c0728e1f470cda6d9862955
MKashanButt <kashanbutt566@gmail.com>
2025-05-30 21:24:27 +0500

    add delete confimation modal

--- resources/views/make/index.blade.php
+++ resources/views/make/index.blade.php
@@ -47,13 +47,34 @@
                                     <a href="{{ route('make.edit', $data) }}">
                                         <x-primary-button>Edit</x-primary-button>
                                     </a>
-                                    <form action="{{ route('make.destroy', $data) }}" method="POST">
+                                    <form action="{{ route('make.destroy', $data) }}" method="POST"
+                                        x-data="{ open: false }">
                                         @method('DELETE')
                                         @csrf
-                                        <x-danger-button
-                                            onclick="confirm('Are you sure you want to delete {{ ucwords($data['name']) }}?')">
+
+                                        <!-- Delete Button - Triggers Modal -->
+                                        <x-danger-button type="button" x-on:click="open = true">
                                             Delete
                                         </x-danger-button>
+
+                                        <!-- Confirmation Modal -->
+                                        <div x-show="open" x-transition
+                                            class="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+                                            <div class="bg-white p-6 rounded-lg max-w-sm w-full">
+                                                <p class="mb-4">Are you sure you want to delete {{ $data['name'] }}?
+                                                </p>
+                                                <div class="flex justify-end space-x-4">
+                                                    <button type="button" x-on:click="open = false"
+                                                        class="px-4 py-2 bg-gray-300 rounded hover:bg-gray-400">
+                                                        Cancel
+                                                    </button>
+                                                    <button type="submit"
+                                                        class="px-4 py-2 bg-red-600 text-white rounded hover:bg-red-700">
+                                                        Confirm Delete
+                                                    </button>
+                                                </div>
+                                            </div>
+                                        </div>
                                     </form>
                                 </div>
                             </td>
