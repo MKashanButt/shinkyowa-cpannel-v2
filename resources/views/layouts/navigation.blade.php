cfd49bd62d00ec028b731213a0674c9718e11ea8
MKashanButt <kashanbutt566@gmail.com>
2025-05-30 21:24:44 +0500

    update routes and add country option

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -90,7 +90,7 @@
                 class="text-xs font-semibold text-[#706f6c] uppercase tracking-wider mb-2 bg-blue-900 p-2 text-white rounded-sm">
                 Website
             </p>
-            <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" class="block py-2">
+            <x-nav-link :href="route('inquiry.index')" :active="request()->routeIs('inquiry.*')" class="block py-2">
                 {{ __('Inquiries') }}
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor"
                     stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-5 h-5 inline-block mr-2">
@@ -105,10 +105,20 @@
                 class="text-xs font-semibold text-[#706f6c] uppercase tracking-wider mb-2 bg-blue-900 p-2 text-white rounded-sm">
                 Settings
             </p>
+            <x-nav-link :href="route('country.index')" :active="request()->routeIs('country.*')" class="block py-2">
+                {{ __('Countries') }}
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                    stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-5 h-5 inline-block mr-2">
+                    <path
+                        d="M3.714 3.048a.498.498 0 0 0-.683.627l2.843 7.627a2 2 0 0 1 0 1.396l-2.842 7.627a.498.498 0 0 0 .682.627l18-8.5a.5.5 0 0 0 0-.904z" />
+                    <path d="M6 12h16" />
+                </svg>
+            </x-nav-link>
             <x-nav-link :href="route('make.index')" :active="request()->routeIs('make.*')" class="block py-2">
                 {{ __('Makes') }}
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor"
-                    stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-5 h-5 inline-block mr-2">
+                    stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                    class="w-5 h-5 inline-block mr-2">
                     <path
                         d="M3.714 3.048a.498.498 0 0 0-.683.627l2.843 7.627a2 2 0 0 1 0 1.396l-2.842 7.627a.498.498 0 0 0 .682.627l18-8.5a.5.5 0 0 0 0-.904z" />
                     <path d="M6 12h16" />
