fef941af34881dffbe54d23fec273ab1ed29cbbe
MKashanButt <kashanbutt566@gmail.com>
2025-05-30 21:18:37 +0500

    add country controller

---
+++ app/Http/Controllers/CountryController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreCountryRequest;
+use App\Http\Requests\UpdateCountryRequest;
+use App\Models\Country;
+use Illuminate\Http\Request;
+
+class CountryController extends Controller
+{
+    public function index()
+    {
+        $countries = Country::withCount('stock')
+            ->paginate(8);
+        return view("country.index", compact('countries'));
+    }
+
+    public function create()
+    {
+        return view('country.create');
+    }
+
+    public function store(StoreCountryRequest $request)
+    {
+        $validated = $request->validated();
+
+        Country::create($validated);
+
+        return redirect()->route('country.index')
+            ->with('success', 'Country created successfully.');
+    }
+
+    public function edit(Country $country)
+    {
+        return view('country.edit', compact('country'));
+    }
+
+    public function update(UpdateCountryRequest $request, Country $country)
+    {
+        $validated = $request->validated();
+
+        $country->update($validated);
+
+        return redirect()->route('country.index')
+            ->with('success', 'Country updated successfully.');
+    }
+
+    public function destroy(Country $country)
+    {
+        $country->delete();
+
+        return redirect()->back()
+            ->with('success', 'Country Deleted');
+    }
+}
