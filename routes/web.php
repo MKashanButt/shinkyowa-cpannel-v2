8ebf4c3162604b45205afc6fdf90c5d3a64098ed
MKashanButt <kashanbutt566@gmail.com>
2025-05-30 21:24:12 +0500

    add inquiry and country routes

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,15 @@
 <?php
 
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\CountryController;
 use App\Http\Controllers\CurrencyController;
 use App\Http\Controllers\CustomerAccountController;
+use App\Http\Controllers\InquiryController;
 use App\Http\Controllers\MakeController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\StockController;
 use App\Models\CustomerAccount;
+use App\Models\Inquiry;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -23,6 +26,8 @@ Route::middleware('auth')->group(function () {
     Route::resource('make', MakeController::class);
     Route::resource('category', CategoryController::class);
     Route::resource('currency', CurrencyController::class);
+    Route::resource('inquiry', InquiryController::class);
+    Route::resource('country', CountryController::class);
 });
 
 require __DIR__ . '/auth.php';
