a52170f24d6065c69ca957383c1d5ff8dad1652c
MKashanButt <kashanbutt566@gmail.com>
2025-06-04 22:38:32 +0500

    add update shipment request

---
+++ app/Http/Requests/UpdateShipmentRequest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rule;
+
+class UpdateShipmentRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return Auth::check()
+            && !Auth::user()->hasRole('customer')
+            && !Auth::user()->hasRole('agent');
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'vessel_name' => 'sometimes|required|string|max:255',
+            'etd' => 'sometimes|required|date',
+            'eta' => 'sometimes|required|date|after_or_equal:etd',
+            'stock_id' => 'sometimes|required|array',
+            'stock_id.*' => [
+                'required_with:stock_id',
+                'integer',
+                Rule::exists('stocks', 'id')
+            ],
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'vessel_name.required' => 'The vessel name field is required.',
+            'eta.required' => 'The estimated time of arrival field is required.',
+            'eta.date' => 'The ETA must be a valid date.',
+            'etd.required' => 'The estimated time of departure field is required.',
+            'etd.date' => 'The ETD must be a valid date.',
+            'etd.after_or_equal' => 'The ETD must be equal to or after the ETA.',
+            'stock_id.required' => 'At least one stock item must be selected.',
+            'stock_id.*.exists' => 'One or more selected stock items are invalid.',
+        ];
+    }
+}
