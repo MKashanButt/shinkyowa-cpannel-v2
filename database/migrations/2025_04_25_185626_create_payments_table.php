cf673769ac1038e155343ff89856ce19c0bbc39d
MKashanButt <kashanbutt566@gmail.com>
2025-06-13 21:25:25 +0500

    add table column

--- database/migrations/2025_04_25_185626_create_payments_table.php
+++ database/migrations/2025_04_25_185626_create_payments_table.php
@@ -19,12 +19,22 @@ return new class extends Migration
                 ->onDelete('set null');
             $table->string('description');
             $table->date('payment_date');
-            $table->integer('in_usd');
+            $table->integer('amount');
             $table->integer('in_yen');
-            $table->date('payment_recieved_date');
+            $table->date('payment_recieved_date')
+                ->nullable();
             $table->foreignId('customer_account_id')
                 ->nullable()
                 ->constrained('customer_accounts')
+                ->onDelete('set null');
+            $table->string('file', 100);
+            $table->enum('status', ['approved', 'not approved'])
+                ->default('not approved');
+            $table->boolean('viewed')
+                ->default(false);
+            $table->foreignId('user_id')
+                ->nullable()
+                ->constrained('users')
                 ->onDelete('set null');
             $table->timestamps();
         });
