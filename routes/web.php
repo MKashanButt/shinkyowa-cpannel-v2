8b332dfe0132bd110b44af9c58a93a0fb1413156
MKashanButt <kashanbutt566@gmail.com>
2025-06-16 19:00:46 +0500

    add search routes and remove un used imports

--- routes/web.php
+++ routes/web.php
@@ -9,11 +9,10 @@ use App\Http\Controllers\DocumentController;
 use App\Http\Controllers\InquiryController;
 use App\Http\Controllers\MakeController;
 use App\Http\Controllers\PaymentController;
+use App\Http\Controllers\PendingTTController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ShipmentController;
 use App\Http\Controllers\StockController;
-use App\Models\CustomerAccount;
-use App\Models\Inquiry;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -26,12 +25,21 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
     Route::resource('customer-account', CustomerAccountController::class);
+    Route::post('customer-account/email', [CustomerAccountController::class, 'searchByEmail'])
+        ->name('customer-account.searchByEmail');
+    Route::post('customer-account/company', [CustomerAccountController::class, 'searchByCompany'])
+        ->name('customer-account.searchByCompany');
+
     Route::resource('reserved-vehicle', ReservedVehicleController::class);
 
     Route::resource('stock', StockController::class);
+    Route::post('stock', [StockController::class, 'search'])
+        ->name('stock.search');
+
     Route::resource('shipment', ShipmentController::class);
     Route::resource('document', DocumentController::class);
     Route::resource('payment', PaymentController::class);
+    Route::resource('pending-tt', PendingTTController::class);
 
     Route::resource('make', MakeController::class);
     Route::resource('category', CategoryController::class);
