86c04daf1a6fb34c3ffc2bd4a4ef04079b5da88e
MKashanButt <kashanbutt566@gmail.com>
2025-06-24 20:26:39 +0500

    add user permissions

--- app/Http/Controllers/PendingTTController.php
+++ app/Http/Controllers/PendingTTController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Payment;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class PendingTTController extends Controller
 {
@@ -10,6 +12,16 @@ class PendingTTController extends Controller
     {
         $payments = Payment::with('customerAccount', 'stock', 'user')
             ->where('status', 'not approved')
+            ->when(Auth::user()->hasPermission('view_team_payments'), function ($query) {
+                $managerAgentIds = User::where('manager_id', Auth::id())
+                    ->where('role', 'agent')
+                    ->pluck('id');
+
+                $query->whereIn('user_id', $managerAgentIds);
+            })
+            ->when(Auth::user()->hasPermission('view_own_payments'), function ($query) {
+                $query->where('user_id', Auth::id());
+            })
             ->paginate(8);
 
         return view('pending-tt.index', compact('payments'));
