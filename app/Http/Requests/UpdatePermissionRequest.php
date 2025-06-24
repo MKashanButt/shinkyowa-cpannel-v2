3518a4ea54ef5d81b40ae0a13c5d834a3629abf5
MKashanButt <kashanbutt566@gmail.com>
2025-06-24 20:29:08 +0500

    add update permission request

---
+++ app/Http/Requests/UpdatePermissionRequest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rule;
+
+class UpdatePermissionRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return Auth::check() && Auth::user()->hasRole('admin');
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string', 'max:255', 'unique:permissions,name,' . Rule::unique('permissions')->ignore($this->permission)],
+            'role_id' => ['required', 'exists:roles,id'],
+        ];
+    }
+
+    /**
+     * Get the custom messages for the validation rules.
+     *
+     * @return array<string, string>
+     */
+
+    public function messages(): array
+    {
+        return [
+            'name.required' => 'The permission name is required.',
+            'name.string' => 'The permission name must be a string.',
+            'name.max' => 'The permission name may not be greater than 255 characters.',
+            'name.unique' => 'The permission name has already been taken.',
+
+            'role_id.required' => 'The role is required.',
+            'role_id.exists' => 'The selected role does not exist.',
+        ];
+    }
+}
