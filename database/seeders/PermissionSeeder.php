22d039b7b96b4317a4a5e8a607ec422d207a9de0
MKashanButt <kashanbutt566@gmail.com>
2025-10-02 00:57:42 +0500

    add permissions for roles

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -13,10 +13,78 @@ class PermissionSeeder extends Seeder
      */
     public function run(): void
     {
+        // Overall Permissions
+        $permission = Permission::create([
+            'name' => 'add_customer',
+        ]);
+
+        $permission->role()->attach([1, 2, 3]);
+
+        $permission = Permission::create([
+            'name' => 'add_payment',
+        ]);
+
+        $permission->role()->attach([1, 2, 3]);
+
+        $permission = Permission::create([
+            'name' => 'reserve_vehicle',
+        ]);
+
+        $permission->role()->attach([1, 2, 3]);
+
+        $permission = Permission::create([
+            'name' => 'view_shipment',
+        ]);
+
+        $permission->role()->attach([1, 2, 3]);
+
+        $permission = Permission::create([
+            'name' => 'view_documents',
+        ]);
+
+        $permission->role()->attach([1, 2, 3]);
+
         $permission = Permission::create([
             'name' => 'view_inquiries',
         ]);
 
-        $permission->role()->attach(1);
+        $permission->role()->attach([1, 2, 3]);
+
+        $permission = Permission::create([
+            'name' => 'view_settings',
+        ]);
+
+        // Admin & Manager Only Permissions
+        $permission->role()->attach([1, 2]);
+
+        $permission = Permission::create([
+            'name' => 'add_shipment',
+        ]);
+
+        $permission->role()->attach([1, 2]);
+
+        $permission = Permission::create([
+            'name' => 'can_edit_shipment',
+        ]);
+
+        $permission->role()->attach([1, 2]);
+
+        $permission = Permission::create([
+            'name' => 'can_delete_shipment',
+        ]);
+
+        $permission->role()->attach([1, 2]);
+
+        $permission = Permission::create([
+            'name' => 'can_edit_document',
+        ]);
+
+        $permission->role()->attach([1, 2]);
+
+        $permission = Permission::create([
+            'name' => 'can_delete_document',
+        ]);
+
+        $permission->role()->attach([1, 2]);
     }
 }
