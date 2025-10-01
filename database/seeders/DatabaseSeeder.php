6d6f0bed6ee4ab69c2883b209503d2519e122786
MKashanButt <kashanbutt566@gmail.com>
2025-10-02 00:57:31 +0500

    add permission seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,6 +20,7 @@ class DatabaseSeeder extends Seeder
             RoleSeeder::class,
             MakeSeeder::class,
             UserSeeder::class,
+            PermissionSeeder::class
         ]);
     }
 }
