f16bb26e6926c0f550bca13f3e95262fb315704a
MKashanButt <kashanbutt566@gmail.com>
2025-10-14 01:39:32 +0500

    add auth facade

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,6 +6,7 @@ use App\Http\Requests\StoreUserRequest;
 use App\Http\Requests\UpdateUserRequest;
 use App\Models\Role;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
