e9963e680d653799fe39275232892f381a300525
MKashanButt <kashanbutt566@gmail.com>
2025-11-19 21:01:26 +0500

    update rendering variable of agents to overallUsers in admin section and made description not required

--- resources/views/customer-account/create.blade.php
+++ resources/views/customer-account/create.blade.php
@@ -68,7 +68,7 @@
                     </div>
                     <div class="flex items-center gap-2 col-span-2">
                         <x-input-label for="description" class="w-[14%]">Description</x-input-label>
-                        <x-text-input type="text " id="description" name="description" required class="w-[90%]"
+                        <x-text-input type="text " id="description" name="description" class="w-[90%]"
                             value="{{ old('description') }}" />
                         <x-input-error :messages="$errors->get('description')" class="mt-2" />
                     </div>
@@ -107,8 +107,7 @@
                         <x-select-box id="country_id" name="country_id" class="w-4/5">
                             <option value="">Select Country</option>
                             @foreach ($countries as $key => $item)
-                                <option value="{{ $key }}"
-                                    {{ old('country_id') == $key ? 'selected' : '' }}>
+                                <option value="{{ $key }}" {{ old('country_id') == $key ? 'selected' : '' }}>
                                     {{ $item }}
                                 </option>
                             @endforeach
@@ -117,24 +116,30 @@
                     </div>
                 </div>
             </div>
-            <div>
-                <h2 class="w-full bg-gray-200/50 my-2 p-2 border-l-2 border-blue-900">Admin</h2>
-                <div class="w-full flex gap-4 flex-wrap">
-                    <div class="flex items-center gap-2 w-[32.5%]">
-                        <x-input-label for="agent_id"
-                            class="w-[32%] after:content-['*'] after:text-red-500">Agent</x-input-label>
-                        <x-select-box id="agent_id" name="agent_id" class="w-4/5">
-                            <option value="">Select Agents</option>
-                            @foreach ($agents as $key => $item)
-                                <option value="{{ $key }}" {{ old('agent_id') == $key ? 'selected' : '' }}>
-                                    {{ $item }}
-                                </option>
-                            @endforeach
-                        </x-select-box>
-                        <x-input-error :messages="$errors->get('agent_id')" class="mt-2" />
+            @if (Auth::user()->hasRole('admin'))
+                <div>
+                    <h2 class="w-full bg-gray-200/50 my-2 p-2 border-l-2 border-blue-900">Admin</h2>
+                    <div class="w-full flex gap-4 flex-wrap">
+                        <div class="flex items-center gap-2 w-[32.5%]">
+                            <x-input-label for="agent_id"
+                                class="w-[32%] after:content-['*'] after:text-red-500">Agent</x-input-label>
+                            <x-select-box id="agent_id" name="agent_id" class="w-4/5">
+                                @if (count($overallUsers) > 0)
+                                    <option value="" selected>Select Agents</option>
+                                @else
+                                    <option value="" selected disabled>No Users</option>
+                                @endif
+                                @foreach ($overallUsers as $key => $item)
+                                    <option value="{{ $key }}" {{ old('agent_id') == $key ? 'selected' : '' }}>
+                                        {{ $item }}
+                                    </option>
+                                @endforeach
+                            </x-select-box>
+                            <x-input-error :messages="$errors->get('agent_id')" class="mt-2" />
+                        </div>
                     </div>
                 </div>
-            </div>
+            @endif
             <x-success-button class="justify-self-end w-[10%] flex justify-center">Add</x-success-button>
         </form>
     </section>
--- resources/views/customer-account/edit.blade.php
+++ resources/views/customer-account/edit.blade.php
@@ -53,8 +53,7 @@
                         <x-select-box id="currency_id" name="currency_id" class="w-4/5">
                             <option value="">Select Currency</option>
                             @foreach ($currencies as $key => $item)
-                                <option value="{{ $key }}"
-                                    {{ old('currency_id', $customerAccount->currency_id) == $key ? 'selected' : '' }}>
+                                <option value="{{ $key }}" {{ old('currency_id', $customerAccount->currency_id) == $key ? 'selected' : '' }}>
                                     {{ $item }}
                                 </option>
                             @endforeach
@@ -101,8 +100,7 @@
                         <x-select-box id="country_id" name="country_id" class="w-4/5">
                             <option value="">Select Country</option>
                             @foreach ($countries as $key => $item)
-                                <option value="{{ $key }}"
-                                    {{ old('country_id', $customerAccount->country_id) == $key ? 'selected' : '' }}>
+                                <option value="{{ $key }}" {{ old('country_id', $customerAccount->country_id) == $key ? 'selected' : '' }}>
                                     {{ $item }}
                                 </option>
                             @endforeach
@@ -111,25 +109,30 @@
                     </div>
                 </div>
             </div>
-            <div>
-                <h2 class="w-full bg-gray-200/50 my-2 p-2 border-l-2 border-blue-900">Admin</h2>
-                <div class="w-full flex gap-4 flex-wrap">
-                    <div class="flex items-center gap-2 w-[32.5%]">
-                        <x-input-label for="agent_id"
-                            class="w-[32%] after:content-['*'] after:text-red-500">Agent</x-input-label>
-                        <x-select-box id="agent_id" name="agent_id" class="w-4/5">
-                            <option value="">Select Agents</option>
-                            @foreach ($agents as $key => $item)
-                                <option value="{{ $key }}"
-                                    {{ old('agent_id', $customerAccount->agent_id) == $key ? 'selected' : '' }}>
-                                    {{ $item }}
-                                </option>
-                            @endforeach
-                        </x-select-box>
-                        <x-input-error :messages="$errors->get('agent_id')" class="mt-2" />
+            @if (Auth::user()->hasRole('admin'))
+                <div>
+                    <h2 class="w-full bg-gray-200/50 my-2 p-2 border-l-2 border-blue-900">Admin</h2>
+                    <div class="w-full flex gap-4 flex-wrap">
+                        <div class="flex items-center gap-2 w-[32.5%]">
+                            <x-input-label for="agent_id"
+                                class="w-[32%] after:content-['*'] after:text-red-500">Agent</x-input-label>
+                            <x-select-box id="agent_id" name="agent_id" class="w-4/5">
+                                @if (count($overallUsers) > 0)
+                                    <option value="" selected>Select Agents</option>
+                                @else
+                                    <option value="" selected disabled>No Users</option>
+                                @endif
+                                @foreach ($overallUsers as $key => $item)
+                                    <option value="{{ $key }}" {{ old('agent_id') == $key ? 'selected' : '' }}>
+                                        {{ $item }}
+                                    </option>
+                                @endforeach
+                            </x-select-box>
+                            <x-input-error :messages="$errors->get('agent_id')" class="mt-2" />
+                        </div>
                     </div>
                 </div>
-            </div>
+            @endif
             <x-success-button class="justify-self-end w-[10%] flex justify-center">Update</x-success-button>
         </form>
     </section>
