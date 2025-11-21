900a6fdb9bf37758f5ed6d29e1c217de3350128d
MKashanButt <kashanbutt566@gmail.com>
2025-11-22 02:26:08 +0500

    update build file link

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,7 +13,7 @@
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
     @if (config('app.env') == 'production')
-        <link rel="stylesheet" href="{{ asset('build/assets/app-OAMpoZUc.css') }}">
+        <link rel="stylesheet" href="{{ asset('build/assets/app-DAaPMldF.css') }}">
         <script src="{{ asset('build/assets/app-Bf4POITK.js') }}"></script>
     @else
         @vite(['resources/css/app.css', 'resources/js/app.js'])
