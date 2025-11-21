3e5503cce0105d6bcf2cd8d62baacc4baea58ecf
MKashanButt <kashanbutt566@gmail.com>
2025-11-22 01:30:31 +0500

    fix auth guard issue

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -28,7 +28,7 @@ class UserController extends Controller
 
     public function create()
     {
-        $roles = Auth::hasRole('admin') ? Role::pluck('name', 'id') : Role::whereNot('name', 'admin')->pluck('name', 'id');
+        $roles = Auth::user()->hasRole('admin') ? Role::pluck('name', 'id') : Role::whereNot('name', 'admin')->pluck('name', 'id');
         $managers = User::whereHas('role', function ($q) {
             $q->where('name', 'manager');
         })->pluck('name', 'id');
