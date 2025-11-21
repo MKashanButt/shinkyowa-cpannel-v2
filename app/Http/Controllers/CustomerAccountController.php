620b5c880d42b3e85d8bb8ed2e81e6e4a577384d
MKashanButt <kashanbutt566@gmail.com>
2025-11-21 23:17:48 +0500

    fix role issue on customer account creation form

--- app/Http/Controllers/CustomerAccountController.php
+++ app/Http/Controllers/CustomerAccountController.php
@@ -46,8 +46,14 @@ class CustomerAccountController extends Controller
     {
         $countries = Country::pluck('name', 'id');
 
-        $agentUsers = User::where('role', 'agent')->pluck('name', 'id');
-        $managerUsers = User::where('role', 'managers')->pluck('name', 'id');
+        $agentUsers = User::whereHas('role', function ($q) {
+            $q->where('name', 'agent');
+        })->pluck('name', 'id');
+
+        $managerUsers = User::whereHas('role', function ($q) {
+            $q->where('name', 'managers');
+        })->pluck('name', 'id');
+
         $overallUsers = $managerUsers->merge($agentUsers);
 
         $currencies = Currency::pluck('code', 'id');
