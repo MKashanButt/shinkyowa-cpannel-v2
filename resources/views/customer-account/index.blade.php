46e48f8494ee00b958700613856d934409ec5ab5
MKashanButt <kashanbutt566@gmail.com>
2025-11-24 19:40:51 +0500

    remove rbac and add permission checking instead on agent name column

--- resources/views/customer-account/index.blade.php
+++ resources/views/customer-account/index.blade.php
@@ -25,7 +25,7 @@
                             Deposit</th>
                         <th class="px-6 py-3 text-left text-xs font-medium text-[#706f6c] uppercase tracking-wider">
                             Remaining</th>
-                        @if (auth()->user()->hasRole('admin') || auth()->user()->hasRole('manager'))
+                        @if (Auth::user()->hasPermission('can_see_agent_name'))
                             <th class="px-6 py-3 text-left text-xs font-medium text-[#706f6c] uppercase tracking-wider">
                                 Agent</th>
                         @endif
