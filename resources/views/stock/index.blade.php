6642a3c081b332b6de7966419fd7c2931f9e5828
MKashanButt <kashanbutt566@gmail.com>
2025-11-26 19:51:44 +0500

    add search to show searched value

--- resources/views/stock/index.blade.php
+++ resources/views/stock/index.blade.php
@@ -15,7 +15,7 @@
                 @csrf
                 <input type="search" name="search" id="search"
                     class="rounded-md text-xs uppercase border border-transparent focus:border-blue-900 focus:ring-blue-900"
-                    placeholder="Search" />
+                    placeholder="Search" value="{{ isset($search) ? $search : '' }}" />
                 <button
                     class="inline-flex items-center px-2 py-1 bg-white font-semibold text-xs text-gray-700 uppercase tracking-widest hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:opacity-25 transition ease-in-out duration-150 rounded-md">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
