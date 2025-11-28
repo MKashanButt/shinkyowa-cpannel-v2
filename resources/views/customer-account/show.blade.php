457a4896a07f83f6e920b0fd891eac096faac60a
MKashanButt <kashanbutt566@gmail.com>
2025-11-28 20:44:09 +0500

    removed fob price and showed cnf price instead

--- resources/views/customer-account/show.blade.php
+++ resources/views/customer-account/show.blade.php
@@ -138,7 +138,7 @@
                                     Year</th>
                                 <th
                                     class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                    FOB Price</th>
+                                    CNF Price</th>
                                 <th
                                     class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                                     Shipment</th>
@@ -160,7 +160,7 @@
                                     <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{{ $stock->year }}
                                     </td>
                                     <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                                        {{ $customerAccount->currency->symbol ?? '$' }}{{ number_format($stock->fob, 2) }}
+                                        {{ $customerAccount->currency->symbol ?? '$' }}{{ number_format($stock->cnf, 2) }}
                                     </td>
                                     <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
                                         {{ $stock->shipment->first()?->vessel_name }}
@@ -245,7 +245,7 @@
                                     <td class="px-6 py-4 whitespace-nowrap text-sm">
                                         <span
                                             class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full 
-                                                                                                                                                                                                                                                                                                            {{ $payment->status == 'approved' ? 'bg-green-100 text-green-800' : 'bg-yellow-100 text-yellow-800' }}">
+                                                                                                                                                                                                                                                                                                                                    {{ $payment->status == 'approved' ? 'bg-green-100 text-green-800' : 'bg-yellow-100 text-yellow-800' }}">
                                             {{ ucfirst($payment->status) }}
                                         </span>
                                     </td>
